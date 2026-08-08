534fdc1a0f0b509c588ac96c45f06693932b29b8
N3vaaa <n3vaaa.spl@gmail.com>
2026-08-08 22:30:33 +0200

    Update iPhoneCredentialProvider.h

--- CredentialProvider/iPhoneCredentialProvider.h
+++ CredentialProvider/iPhoneCredentialProvider.h
@@ -1,1 +1,53 @@
+#pragma once
 
+#include <windows.h>
+#include <credentialprovider.h>
+
+class IPhoneCredentialProvider : public ICredentialProvider
+{
+public:
+    IPhoneCredentialProvider();
+    virtual ~IPhoneCredentialProvider();
+
+    // IUnknown
+    IFACEMETHODIMP QueryInterface(
+        REFIID riid,
+        void** ppv) override;
+
+    IFACEMETHODIMP_(ULONG) AddRef() override;
+
+    IFACEMETHODIMP_(ULONG) Release() override;
+
+    // ICredentialProvider
+    IFACEMETHODIMP SetUsageScenario(
+        CREDENTIAL_PROVIDER_USAGE_SCENARIO cpus,
+        DWORD dwFlags) override;
+
+    IFACEMETHODIMP SetSerialization(
+        const CREDENTIAL_PROVIDER_CREDENTIAL_SERIALIZATION* pcpcs) override;
+
+    IFACEMETHODIMP Advise(
+        ICredentialProviderEvents* pcpe,
+        UINT_PTR upAdviseContext) override;
+
+    IFACEMETHODIMP UnAdvise() override;
+
+    IFACEMETHODIMP GetFieldDescriptorCount(
+        DWORD* pdwCount) override;
+
+    IFACEMETHODIMP GetFieldDescriptorAt(
+        DWORD dwIndex,
+        CREDENTIAL_PROVIDER_FIELD_DESCRIPTOR** ppcpfd) override;
+
+    IFACEMETHODIMP GetCredentialCount(
+        DWORD* pdwCount,
+        DWORD* pdwDefault,
+        BOOL* pbAutoLogonWithDefault) override;
+
+    IFACEMETHODIMP GetCredentialAt(
+        DWORD dwIndex,
+        ICredentialProviderCredential** ppcpc) override;
+
+private:
+    LONG _refCount;
+};
